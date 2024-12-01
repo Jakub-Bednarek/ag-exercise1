c3f54b5263c9a6b011f6fb7f83e9e6dc8be05465
Jakub Bednarek <jakub.bednarek1009@gmail.com>
2024-12-01 16:42:28 +0100

    Created ProgramData and DataEntry classes with basic representation of problem solved, added definition of load_data function to load content of input file with safety checks

---
+++ helpers/data_loader.py
@@ -0,0 +1,48 @@
+import os.path
+
+
+class InvalidInputFilePathException(Exception):
+    pass
+
+
+class ProgramData:
+    class DataEntry:
+        def __init__(self, value, weight):
+            self.value = value
+            self.weight = weight
+
+        def __str__(self):
+            return f"{self.value} {self.weight}"
+
+    def __init__(self):
+        self.objects_count: int = 0
+        self.storage_size: int = 0
+        self.data_set: Array[DataEntry] = []
+
+
+def load_data(file_path: str):
+    file_path = os.path.abspath(file_path)
+
+    file_content = []
+    with open(file_path, "r") as file_stream:
+        file_content = file_stream.readlines()
+
+    if len(file_content) == 0:
+        raise EmptyInputException()
+
+    program_data = ProgramData()
+    objects_count, storage_size = file_content[0].split()
+    program_data.objects_count = objects_count
+    program_data.storage_size = storage_size
+
+    for data_entry in file_content[1:]:
+        if not data_entry:
+            continue
+
+        item_value, item_weight = data_entry.split()
+        program_data.data_set.append(ProgramData.DataEntry(item_value, item_weight))
+
+    for item in program_data.data_set:
+        print(item)
+
+    return program_data
--- run.py
+++ run.py
@@ -1,7 +1,21 @@
 #!/bin/python3
 
+from helpers.data_loader import load_data, InvalidInputFilePathException
+
 def main():
     print("Hello there")
+
+    loaded_data = None
+    try:
+        loaded_data = load_data("./data/low-dimensional/file.txt")
+    except Exception as e:
+        print(e)
+        return 1
+
+    print(loaded_data.objects_count)
+    print(loaded_data.storage_size)
+    print(len(loaded_data.data_set))
+
 
 if __name__ == "__main__":
     main()
