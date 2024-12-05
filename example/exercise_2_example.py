85553393957eab612382dca7dd7a304e1f2f7e58
Jakub Bednarek <jakub.bednarek1009@gmail.com>
2024-12-05 02:24:00 +0100

    Added base for exercise_2 experiments

---
+++ example/exercise_2_example.py
@@ -0,0 +1,45 @@
+
+DATA_PATH = "data/"
+SMALL_DATA_PATH = f"{DATA_PATH}/low-dimensional/"
+LARGE_DATA_PATH = f"{DATA_PATH}/large_scale/"
+
+SMALL_DATA_FILE_0 = f"{SMALL_DATA_PATH}f1_1-d_kp_10_269"
+SMALL_DATA_FILE_1 = f"{SMALL_DATA_PATH}f2_1-d_kp_20_878"
+SMALL_DATA_FILE_2 = f"{SMALL_DATA_PATH}f3_1-d_kp_4_20"
+SMALL_DATA_FILE_3 = f"{SMALL_DATA_PATH}f4_1-d_kp_4_11"
+SMALL_DATA_FILE_4 = f"{SMALL_DATA_PATH}f5_1-d_kp_15_375"
+SMALL_DATA_FILE_5 = f"{SMALL_DATA_PATH}f6_1-d_kp_10_60"
+SMALL_DATA_FILE_6 = f"{SMALL_DATA_PATH}f7_1-d_kp_7_50"
+SMALL_DATA_FILE_7 = f"{SMALL_DATA_PATH}f8_1-d_kp_23_10000"
+SMALL_DATA_FILE_8 = f"{SMALL_DATA_PATH}f9_1-d_kp_5_80"
+SMALL_DATA_FILE_9 = f"{SMALL_DATA_PATH}f10_1-d_kp_20_879"
+
+LARGE_DATA_FILE_0 = f"{LARGE_DATA_PATH}/knapPI_1_10000_1000_1"
+LARGE_DATA_FILE_1 = f"{LARGE_DATA_PATH}/knapPI_1_1000_1000_1"
+LARGE_DATA_FILE_2 = f"{LARGE_DATA_PATH}/knapPI_1_100_1000_1"
+LARGE_DATA_FILE_3 = f"{LARGE_DATA_PATH}/knapPI_1_2000_1000_1"
+LARGE_DATA_FILE_4 = f"{LARGE_DATA_PATH}/knapPI_1_200_1000_1"
+LARGE_DATA_FILE_5 = f"{LARGE_DATA_PATH}/knapPI_1_5000_1000_1"
+LARGE_DATA_FILE_6 = f"{LARGE_DATA_PATH}/knapPI_1_500_1000_1"
+LARGE_DATA_FILE_7 = f"{LARGE_DATA_PATH}/knapPI_2_1000_1000_1"
+LARGE_DATA_FILE_8 = f"{LARGE_DATA_PATH}/knapPI_2_100_1000_1"
+LARGE_DATA_FILE_9 = f"{LARGE_DATA_PATH}/knapPI_2_2000_1000_1"
+LARGE_DATA_FILE_10 = f"{LARGE_DATA_PATH}/knapPI_2_200_1000_1"
+LARGE_DATA_FILE_11 = f"{LARGE_DATA_PATH}/knapPI_2_500_1000_1"
+
+OUTPUT_DIR = "output/"
+
+def run_simulation_on_all_test_data_files():
+    pass
+
+def run_mutation_and_crossover_simulation():
+    pass
+
+def run_rank_and_roulette_simulation():
+    pass
+
+def run_single_and_double_point_crossover_simulation():
+    pass
+
+def run_rank_roulette_and_tournament_simulation():
+    pass
